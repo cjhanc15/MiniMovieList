f54ff8d804410e52b33ba49c7357d292c80624fc
Cybyl Jade <cybyljade@Cybyls-MacBook-Pro.local>
2022-06-23 12:30:13 -0600

    level 2 completed

--- front-end/movies/src/MovieList.js
+++ front-end/movies/src/MovieList.js
@@ -1,24 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [searchInput, setSearchInput] = useState('')
+
   useEffect(() => {
     fetch('http://localhost:8080/movies')
     .then(res => res.json())
     .then(data => setMovies(data))
   }, [])
 
-  console.log(movies)
+  const searchHandler = (e) => {
+    setSearchInput(e.target.value)
+  };
+  let filteredResults = movies.filter(movie => {
+    return movie.title.toUpperCase().includes(searchInput.toUpperCase())
+  })
+
   return (
     <div>
-          <h1>Elvis Presley's Movies</h1>
-    <div>
-      {movies.map(movie => (
-        <div>
-          <p>{movie.title}</p>
-        </div>
-      ))}
-    </div>
+      <h1>Elvis Presley's Movies</h1>
+      <div>
+        <input type="text" placeholder="Search" onKeyUp={(e) => searchHandler(e)} />
+        {filteredResults.map(movie => (
+          <div>
+            <p>{movie.title}, {movie.release_year}</p>
+            <img src={movie.cover} alt={movie.title} height='500px'/>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
