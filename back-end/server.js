5144d77e46e988ea4a18c51d4bbdf983ec6ba2b7
Cybyl Jade <cybyljade@Cybyls-MacBook-Pro.local>
2022-06-23 22:04:43 -0600

    add movies fully functional, delete movies started

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV||'development']);
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json());
 
+//CREATE ------------------------------------------------------//
+app.post('/movies', async (req, res) => {
+  await knex('movielist')
+  .insert(req.body)
+  let result = await knex('movielist')
+  .select('*')
+  res.send(result);
+})
+
+// READ -------------------------------------------------------//
 app.get('/', (req, res) => {
   res.send('Movies!')
 });
@@ -18,5 +31,20 @@ app.get('/movies', (req, res) => {
     })
   );
 });
+
+// //UPDATE ------------------------------------------------------//
+// app.patch('/movies', (req, res) => {
+// })
+
+// DELETE ------------------------------------------------------//
+app.delete('/movies', async (req, res) => {
+let result = await knex('movielist')
+  .del()
+  .where(req.body.id)
+await knex('movielist')
+  .select('*')
+  res.status(200)
+  res.send(result);
+})
 
 module.exports = app;
--- front-end/movies/src/MovieList.js
+++ front-end/movies/src/MovieList.js
@@ -1,21 +1,59 @@
 import { useEffect, useState } from "react";
 
 const MovieList = () => {
+  //Hooks
   const [movies, setMovies] = useState([]);
   const [searchInput, setSearchInput] = useState('')
-
+  //Fetch
   useEffect(() => {
     fetch('http://localhost:8080/movies')
     .then(res => res.json())
     .then(data => setMovies(data))
   }, [])
-
+  //Helper Functions
   const searchHandler = (e) => {
     setSearchInput(e.target.value)
   };
   let filteredResults = movies.filter(movie => {
     return movie.title.toUpperCase().includes(searchInput.toUpperCase())
   })
+
+  const addMovie = (title, genre, release_year, cover) => {
+    const newMovie = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        title: title,
+        genre: genre,
+        release_year: parseFloat(release_year),
+        cover: cover
+      })
+    }
+    fetch('http://localhost:8080/movies', newMovie)
+    .then(res => res.json())
+    .then(data => {
+      setMovies(data);
+    })
+  }
+
+  const deleteMovie = (id) => {
+    const deletedMovie = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+    }
+    fetch('http://localhost:8080/movies', deletedMovie)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let newList = {...movies};
+
+      setMovies(newList);
+    })
+  }
 
   return (
     <div>
@@ -26,9 +64,25 @@ const MovieList = () => {
           <div>
             <p>{movie.title}, {movie.release_year}</p>
             <img src={movie.cover} alt={movie.title} height='500px'/>
+            <button onClick={deleteMovie(movie.id)}>Delete</button>
           </div>
         ))}
       </div>
+      <h3>Not finding a movie? Add it here!</h3>
+      <div>
+        <input id='title' placeholder='Title'/>
+        <input id='genre' placeholder='Genre'/>
+        <input id='release_year' placeholder='Year Released'/>
+        <input id='cover' placeholder='Cover'/>
+        <button onClick={() => {
+          addMovie(
+            document.getElementById('title').value,
+            document.getElementById('genre').value,
+            document.getElementById('release_year').value,
+            document.getElementById('cover').value,
+          )
+        }}>Add Movie</button>
+      </div>          
     </div>
   )
 }
